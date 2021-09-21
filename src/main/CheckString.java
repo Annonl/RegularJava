428acefdfff8c3ae99385c7b6ad5dfe0edd6167a
Alexey <alesha.kolpashikov@gmail.com>
2021-09-21 20:46:44 +0300

    Add checks IpAdress, GUID, URL, StrongPassword

--- src/main/CheckString.java
+++ src/main/CheckString.java
@@ -1,5 +1,18 @@
 package main;
 
-public class CheckString {
+import java.util.regex.Pattern;
 
+public class CheckString {
+    public static boolean isIpAdress(String ex){
+        return Pattern.matches("^((\\d{1,2}|1\\d{0,2}|2\\d?|2[0-4]\\d{1}|25[0-5]{1})\\.){3}((\\d{1,2}|1\\d{0,2}|2\\d{0,1}|2[0-4]\\d{1}|25[0-5]{1}))$",ex);
+    }
+    public static boolean isGUID(String ex){
+        return Pattern.matches("^(?:[0-9a-fA-F]{8}-(?:[0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12})$|^\\{(?:[0-9a-fA-F]{8}-(?:[0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12})}$",ex);
+    }
+    public static boolean isURL(String ex){
+        return Pattern.matches("(?!.*([\\.=\\?\\&])\\1)^(http[s]?:\\/\\/)?(?!.*(_))(?!.*(\\.)\\1)\\w{1}[\\w\\.-]+\\.[\\w]*\\w{1}(\\/[\\w\\/\\-\\&\\?\\=]*(\\:[\\d]+)?#?)?$",ex);
+    }
+    public static boolean isStrongPassword(String ex){
+        return Pattern.matches("(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])[\\w-]{8,}",ex);
+    }
 }
